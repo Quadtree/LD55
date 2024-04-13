62314e6acdcd818c60a54afdfb18a6deac9295ce
Quadtree <quadtree@gmail.com>
2024-04-12 20:46:11 -0700

    U

--- actors/PCFireElemental.cs
+++ actors/PCFireElemental.cs
@@ -10,6 +10,10 @@ public class PCFireElemental : Spatial
 
     public override void _Process(float delta)
     {
-
+        if (Input.IsActionPressed("move"))
+        {
+            var dest = Picking.PickPointAtCursor(this);
+            GD.Print(dest);
+        }
     }
 }
