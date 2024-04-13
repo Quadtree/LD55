06f44b7e92f9ad80ae21499155500a59adeb767b
Quadtree <quadtree@gmail.com>
2024-04-12 22:47:07 -0700

    U

--- components/Flammable.cs
+++ components/Flammable.cs
@@ -3,9 +3,7 @@ using System;
 
 public class Flammable : Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    public float Heat = 0;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
