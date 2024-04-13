0732863b07836c09c7e8b767549662cf4a651b84
Quadtree <quadtree@gmail.com>
2024-04-12 20:42:55 -0700

    U

---
+++ actors/PCFireElemental.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class PCFireElemental : Spatial
+{
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(float delta)
+    {
+
+    }
+}
