64d355ee31fe084916806fe9f28fb72a0c33070a
Quadtree <quadtree@gmail.com>
2024-04-13 16:37:08 -0700

    U

--- maps/InterLevelState.cs
+++ maps/InterLevelState.cs
@@ -10,4 +10,5 @@ public class InterLevelState
     public int Difficulty => Level * 2 + 3;
 
     public int SummonerSkill => (int)(25 + Difficulty * 3);
+    public int PlayerBreakBonus => BreakUpgrades * 20;
 }
