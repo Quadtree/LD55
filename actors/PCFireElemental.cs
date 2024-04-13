fa0596a5f1ddec013a0b86fb515b17151d0d54d9
Quadtree <quadtree@gmail.com>
2024-04-12 22:33:19 -0700

    U

--- actors/PCFireElemental.cs
+++ actors/PCFireElemental.cs
@@ -5,6 +5,8 @@ public class PCFireElemental : KinematicBody
 {
     public float Mana = 50;
 
+    public float GlobalCooldown = 0;
+
     [Export]
     public float ManaRegenPerSecond = 20f;
 
@@ -36,15 +38,52 @@ public class PCFireElemental : KinematicBody
             }
         }
 
+        if (Input.IsActionPressed("action") && GlobalCooldown >= 1)
+        {
+            var dest = Picking.PickPointAtCursor(this);
+
+            if (dest != null)
+            {
+                GD.Print($"Dist={dest.Value.DistanceTo(GlobalTranslation)}");
+
+                if (dest.Value.DistanceTo(GlobalTranslation) < 2.5f)
+                {
+                    GD.Print("Flame slash!");
+                    FlameSlash();
+                }
+                else
+                {
+                    GD.Print("Fireball!");
+                }
+            }
+        }
+
+        GlobalCooldown = Util.Clamp(GlobalCooldown + delta, 0, 1);
+
         Mana = Util.Clamp(Mana + ManaRegenPerSecond * delta, 0, 100);
     }
 
     public void FlameSlash()
     {
+
+
         // var pp = new PhysicsShapeQueryParameters();
         // pp.ShapeRid = this.FindChildByName<CollisionShape>("FlameSlashShape").Shape.GetRid();
         // pp.CollisionMask
 
         // GetWorld().DirectSpaceState.IntersectShape()
+
+        if (Mana < 20) return;
+        if (GlobalCooldown < 1) return;
+
+        Mana -= 20;
+        GlobalCooldown = 0;
+
+
+
+        foreach (var it in this.FindChildByType<Area>().GetOverlappingBodies())
+        {
+            GD.Print(it);
+        }
     }
 }
