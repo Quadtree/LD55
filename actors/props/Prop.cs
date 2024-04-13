b4f68ae600dd557c7a4739ad3aa8850a8d66c795
Quadtree <quadtree@gmail.com>
2024-04-13 11:46:37 -0700

    U

--- actors/props/Prop.cs
+++ actors/props/Prop.cs
@@ -3,13 +3,17 @@ using System;
 
 public class Prop : MeshInstance, Actor
 {
-    public Spatial AsSpatial => this;
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
 
+    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         GD.Print("PROP!");
     }
 
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
         if (this.FindChildByType<Damagable>().Health <= 0)
