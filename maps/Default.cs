4fa8e12c56a4cc31c06d3e357ab6b5f645c210a4
Quadtree <quadtree@gmail.com>
2024-04-13 14:40:20 -0700

    U

---
+++ maps/Default.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class Default : Spatial
+{
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(float delta)
+    {
+
+    }
+}
