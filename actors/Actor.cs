5d669b09c0705330b18071727af0f2efd9caccc4
Quadtree <quadtree@gmail.com>
2024-04-12 23:00:07 -0700

    U

--- actors/Actor.cs
+++ actors/Actor.cs
@@ -1,4 +1,15 @@
+using Godot;
+
 public interface Actor
 {
 
+}
+
+public static class ActorExtensions
+{
+    public static Actor GetActor(this Node self)
+    {
+        if (self is Actor) return (Actor)self;
+        return self.FindParentByType<Actor>();
+    }
 }
