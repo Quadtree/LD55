795cc12b80592c5e0a4b68beeda553cfc05a2f3b
Quadtree <quadtree@gmail.com>
2024-04-13 14:28:55 -0700

    U

--- actors/WardingRune.cs
+++ actors/WardingRune.cs
@@ -5,15 +5,24 @@ public class WardingRune : StaticBody
 {
     public float Health = 10;
 
+    public SpatialMaterial LocalMat;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        LocalMat = this.FindChildByType<MeshInstance>().GetActiveMaterial(0).Duplicate() as SpatialMaterial;
+        this.FindChildByType<MeshInstance>().MaterialOverride = (LocalMat);
     }
 
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        if (Health <= 0) QueueFree();
+        LocalMat.EmissionEnergy = Health * 0.25f;
+
+        if (Health <= 0)
+        {
+            CollisionLayer = 0;
+            CollisionMask = 0;
+        }
     }
 }
