698e842b5086f96e8eaecfe71c15548a80fa1471
Quadtree <quadtree@gmail.com>
2024-04-13 16:34:11 -0700

    U

---
+++ ui/UpgradeBox.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class UpgradeBox : VBoxContainer
+{
+    [Export]
+    string TextTemplate;
+
+    public override void _Ready()
+    {
+
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+
+    }
+}
