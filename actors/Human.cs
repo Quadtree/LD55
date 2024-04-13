1498b5e369b220a00a872d092268b15e3b173ceb
Quadtree <quadtree@gmail.com>
2024-04-13 00:30:11 -0700

    U

--- actors/Human.cs
+++ actors/Human.cs
@@ -3,19 +3,34 @@ using System;
 
 public class Human : KinematicBody, Actor
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    Vector3 BurnRun;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-
+        BurnRun = new Vector3(
+                    Util.RandF(-1, 1),
+                    0,
+                    Util.RandF(-1, 1)
+                ).Normalized();
     }
 
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
+        if (this.FindChildByType<Flammable>()?.IsOnFire == true)
+        {
+            if (Util.RandChance(delta))
+            {
+                BurnRun = new Vector3(
+                    Util.RandF(-1, 1),
+                    0,
+                    Util.RandF(-1, 1)
+                ).Normalized();
+            }
 
+            MoveAndSlide(BurnRun * 4);
+            return;
+        }
     }
 }
